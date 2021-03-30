dd753475f0ed167b3a25380a064c87092eb18f71
Shane Peterson <speterson@Shanes-MacBook-Pro.local>
2021-03-29 18:00:14 -0600

    finish 2nd part Django lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Dog
 
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+def about(request):
+    return render(request, 'about.html')
+def dogs_index(request):
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog' : dog})
