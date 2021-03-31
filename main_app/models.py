bcd220c3064b960fea2e631db93a575ed55ac4b0
Shane Peterson <speterson@Shanes-MacBook-Pro.local>
2021-03-30 20:06:44 -0600

    complete django class views

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+    # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
 # Create your views here
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
 def dogs_index(request):
@@ -13,3 +13,15 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog' : dog})
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
